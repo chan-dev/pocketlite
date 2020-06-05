f998cc4dc1b9d12c76389f270b816ff435049d14
chan-dev <christian.m.austria@gmail.com>
2020-06-06 06:49:28 +0800

    feat(tags.component): allow only 1 tag to edit at a time
    Summary:
    * we autofocus the input when we're on tag edit view
    * we used templateRef(#tagEditInput) on tag edit input so we can autofocus on it
    * we used  cdkMonitorSubtreeFocus directive from angular/cdk so we can catch focus/blur events

--- src/app/core/layout/tags/tags.component.ts
+++ src/app/core/layout/tags/tags.component.ts
@@ -27,11 +27,25 @@ export class TagsComponent implements OnInit {
     this.tagControl.setValue(this.tag.name);
   }
 
-  openEdit() {
+  openEdit(tagInput) {
     this.isEditing = true;
+    // NOTE: this autofocuses the input when editing
+    // but it is also required so blur event gets invoked
+    // even if we focus on other non-form elements w/ the help
+    // of cdkMonitorSubtreeFocus directive
+    setTimeout(() => {
+      tagInput.focus();
+    }, 0);
   }
 
   closeEdit() {
     this.isEditing = false;
+  }
+
+  blur(event) {
+    // 'null' means the element is blurred
+    if (event === null) {
+      this.closeEdit();
+    }
   }
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
   declarations: [],
-  exports: [MatSidenavModule, MatIconModule, MatTooltipModule],
+  exports: [MatSidenavModule, MatIconModule, MatTooltipModule, A11yModule],
 })
 export class MaterialModule {}
