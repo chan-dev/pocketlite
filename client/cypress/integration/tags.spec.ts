6e1a71334c1b4701e73f6692e59178d127a5be2c
chan-dev <christian.m.austria@gmail.com>
2020-06-07 15:16:40 +0800

    test: add test for tags

---
+++ client/cypress/integration/tags.spec.ts
@@ -0,0 +1,61 @@
+describe('Tags', () => {
+  beforeEach(() => {
+    cy.viewport('ipad-2');
+    cy.server();
+    cy.route('/api/tags', 'fixture:tags');
+    cy.visit('/');
+    cy.get('[data-cy="tags-menu-button"]').click();
+  });
+
+  it('shows the tag view', () => {
+    cy.get('[data-cy="sidenav-parent-view"]').should('not.exist');
+    cy.get('[data-cy="sidenav-child-view"]').should('exist');
+  });
+
+  it('shows all available tags', () => {
+    // TODO:
+    cy.fixture('tags').then(tags => {
+      cy.get('[data-cy="tags-list"]').as('tags-list');
+
+      cy.get('@tags-list')
+        .find('li')
+        .each(($li, i) => {
+          cy.wrap($li).should('contain.text', tags[i].name);
+        });
+
+      cy.get('@tags-list').should('exist').find('li').should('have.length', 3);
+    });
+  });
+
+  it('shows tag edit view on click', () => {
+    cy.get('[data-cy="tags-list"]')
+      .find('[data-cy="tag-show-view"]')
+      .first()
+      .find('[data-cy="tag-edit-button"]')
+      .invoke('show')
+      .click();
+
+    cy.get('[data-cy="tag-edit-view"]').should('be.visible');
+  });
+
+  it('shows only 1 tag edit view at a time', () => {
+    cy.get('[data-cy="tags-list"]')
+      .find('[data-cy="tag-show-view"]')
+      .each(($el, i) => {
+        cy.wrap($el).find('[data-cy="tag-edit-button"]').invoke('show').click();
+      });
+
+    // we're using [hidden] instead of *ngIf directive so
+    // we have to filter only the visible ones
+    cy.get('[data-cy="tag-edit-view"]:visible').should('have.length', 1);
+  });
+
+  it('autofocus the input when tag edit view is shown', () => {
+    cy.get('[data-cy="tags-list"]')
+      .find('[data-cy="tag-show-view"]')
+      .each(($el, i) => {
+        cy.wrap($el).find('[data-cy="tag-edit-button"]').invoke('show').click();
+        cy.get('[data-cy="tag-edit-view"] input').should('be.focused');
+      });
+  });
+});
