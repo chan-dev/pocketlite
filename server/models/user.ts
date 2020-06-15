80fb155926b9f3773174296d2c1fbb8c4c17cccf
chan-dev <christian.m.austria@gmail.com>
2020-06-15 10:27:54 +0800

    feat(server): update user model

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,8 +1,26 @@
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
   displayName: {
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  thumbnail: {
+    type: String,
+  },
+  provider: {
+    type: String,
+    require: true,
+  },
 });
+
+export default mongoose.model('User', userSchema);
