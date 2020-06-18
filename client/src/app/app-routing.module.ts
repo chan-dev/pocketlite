deab3cd4b87fe1a3734119290fa30d03249ba79a
chan-dev <christian.m.austria@gmail.com>
2020-06-18 13:55:08 +0800

    feat: setup main routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    pathMatch: 'full',
+    // canActivate: [],
+    // children: []
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./core/auth/auth.module').then(m => m.AuthModule),
+  },
+  // TODO: create a not found component
+  // {
+  //   path: '**',
+  //   component: NotFoundComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
