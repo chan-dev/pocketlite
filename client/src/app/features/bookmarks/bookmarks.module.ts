2b4a48e265511e3b1fb1e78c9188718d2fac38bf
chan-dev <christian.m.austria@gmail.com>
2020-08-27 12:10:20 +0800

    refactor(frontend): remove LayoutModule and update BookmarkModule
    Move imports of components to BookmarkModule

--- client/src/app/features/bookmarks/bookmarks.module.ts
+++ client/src/app/features/bookmarks/bookmarks.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { LayoutModule } from './components/layout.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MaterialModule } from 'src/app/shared/material.module';
 import { BookmarksRoutingModule } from './bookmarks-routing.module';
 
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { TagsComponent } from './components/tags/tags.component';
+import { TagsListComponent } from './components/tags/tags-list.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { AddLinkFormComponent } from './components/add-link-form/add-link-form.component';
+
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, LayoutModule, BookmarksRoutingModule],
+  declarations: [
+    MainLayoutComponent,
+    HeaderComponent,
+    SidenavComponent,
+    TagsComponent,
+    TagsListComponent,
+    NotFoundComponent,
+    SearchBoxComponent,
+    AddLinkFormComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SharedModule,
+    MaterialModule,
+    BookmarksRoutingModule,
+  ],
 })
 export class BookmarksModule {}
