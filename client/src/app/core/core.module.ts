3d5bbfe4161146572e4b4a7ba18f4d24664eb8e2
chan-dev <christian.m.austria@gmail.com>
2020-08-29 10:18:39 +0800

    feat(frontend): setup router state

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -7,12 +7,16 @@ import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 
 import { AuthModule } from './auth/auth.module';
 
 import { environment } from 'src/environments/environment';
+
+import { reducers } from './core.state';
+import { CustomSerializer } from './helpers/custom-serializer';
 
 @NgModule({
   declarations: [NotFoundComponent],
@@ -29,7 +33,10 @@ import { environment } from 'src/environments/environment';
       headerName: 'X-XSRF-TOKEN',
     }),
     // we don't have state available upfront
-    StoreModule.forRoot({}),
+    StoreModule.forRoot(reducers),
+    StoreRouterConnectingModule.forRoot({
+      serializer: CustomSerializer,
+    }),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       name: 'PocketLite',
