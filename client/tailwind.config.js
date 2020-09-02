84775d4edbcc953381ee6ebac5c7c8fc5c885ad7
chan-dev <christian.m.austria@gmail.com>
2020-09-02 23:28:13 +0800

    fix(client): use plugin to fix truncate issue
    this allows to truncate "multi-line" text instead of just truncating the text in a single line

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,13 +2,22 @@ module.exports = {
   important: true,
   purge: [],
   theme: {
-    extend: {
+    truncate: {
+      lines: {
+        2: '2',
+        3: '3',
+        5: '5',
+        8: '8',
+      },
     },
+    extend: {},
   },
   variants: {
     display: ['responsive', 'hover', 'focus', 'group-hover'],
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-truncate-multiline')()
+  ],
   future: {
     removeDeprecatedGapUtilities: true,
   },
