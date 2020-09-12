3b287697b25a8b1afacc54e1277f7482f4bc5f04
chan-dev <christian.m.austria@gmail.com>
2020-09-12 19:24:11 +0800

    fix(client): listen to queryparams selector
    This solves the issue of same route navigation wherein it doesn't invoke code in ngOnInit/constructor since Angular reuses the component on same route.
    
    We listen to every queryParams change from ngrx/route-store selector and dispatch both clearBookmarks and searchBookmark actions

--- client/src/app/features/bookmarks/containers/bookmark-search-results-container/bookmark-search-results-container.component.ts
+++ client/src/app/features/bookmarks/containers/bookmark-search-results-container/bookmark-search-results-container.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  OnDestroy,
+} from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
+import { filter, withLatestFrom, concatMap } from 'rxjs/operators';
 
 import { Bookmark } from '@models/bookmark.model';
 import * as fromSearch from '@app/features/bookmarks/state';
-import { tap, skip } from 'rxjs/operators';
+import * as fromRoot from '@app/core/core.state';
 
 @Component({
   selector: 'app-bookmark-search-results-container',
@@ -12,19 +18,44 @@ import { tap, skip } from 'rxjs/operators';
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BookmarkSearchResultsContainerComponent implements OnInit {
-  private page = 1;
+export class BookmarkSearchResultsContainerComponent
+  implements OnInit, OnDestroy {
+  private subscription: Subscription;
+
   bookmarks$: Observable<Bookmark[]>;
 
   constructor(private store: Store) {
     this.bookmarks$ = this.store.pipe(
-      select(fromSearch.selectSearchResults),
-      tap(_ => console.log('fetch search results'))
+      select(fromSearch.selectSearchLoading),
+      filter(loading => loading === false),
+      concatMap(loading =>
+        of(loading).pipe(
+          withLatestFrom(
+            this.store.select(fromSearch.selectSearchResults),
+            (_, bookmarks) => {
+              return bookmarks;
+            }
+          )
+        )
+      )
     );
   }
 
   ngOnInit() {
-    this.store.dispatch(fromSearch.clearBookmarks());
-    this.store.dispatch(fromSearch.searchBookmark());
+    this.subscription = this.store
+      .pipe(
+        select(fromRoot.selectQueryParams),
+        filter(params => params.hasOwnProperty('query'))
+      )
+      .subscribe(params => {
+        this.store.dispatch(fromSearch.clearBookmarks());
+        this.store.dispatch(fromSearch.searchBookmark());
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
