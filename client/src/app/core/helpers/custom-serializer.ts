b091c0e629666c61ad8f7f5163a77b1e61326bd2
chan-dev <christian.m.austria@gmail.com>
2020-10-10 16:53:36 +0800

    fix(client): fix infinite loop on route guards #8
    Basically, everytime the route is an empty string replace it with "/"
    
    Related issue:
    https://github.com/ngrx/platform/issues/1781#issuecomment-489240115
    
    Fixes issue: #8

--- client/src/app/core/helpers/custom-serializer.ts
+++ client/src/app/core/helpers/custom-serializer.ts
@@ -23,6 +23,15 @@ export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
 
     // Only return an object including the URL, params and query params
     // instead of the entire snapshot
-    return { url, params, queryParams };
+    return {
+      // NOTE: this is important, we must provide "/" as a replacement everytime
+      // the route is equals an "" empty route to prevent infinite loop
+      // on guards that redirect to the same component
+      // Check issue here
+      // https://github.com/ngrx/platform/issues/1781#issuecomment-489240115
+      url: url || '/',
+      params,
+      queryParams,
+    };
   }
 }
