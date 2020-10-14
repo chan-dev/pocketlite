ef412ede3bfe75378c3546621eba3faf2bbc8a84
chan-dev <christian.m.austria@gmail.com>
2020-10-14 14:06:26 +0800

    fix(client): reset flag that tracks refresh token progress

--- client/src/app/core/interceptors/token-interceptor.ts
+++ client/src/app/core/interceptors/token-interceptor.ts
@@ -20,7 +20,7 @@ import * as fromAuth from '@app/core/auth/state';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  private isRefreshing = false;
+  private isRefreshInProgress = false;
   private isTokenRefreshed$: Observable<boolean | null>;
 
   constructor(private store: Store) {
@@ -47,21 +47,30 @@ export class TokenInterceptor implements HttpInterceptor {
   }
 
   private handleAuthError(req: HttpRequest<any>, next: HttpHandler) {
-    if (!this.isRefreshing) {
-      this.isRefreshing = true;
+    if (!this.isRefreshInProgress) {
+      this.isRefreshInProgress = true;
       this.store.dispatch(fromAuth.refreshToken());
 
+      // We can't use finalize operator here since it requires
+      // observable completion
+      //
+      // this is a selector that we should keep listening
+      // to it during the lifetime of the session, so we just have
+      // to reset this.isRefreshing in two different places on
+      // switchMap and catchError
       return this.isTokenRefreshed$.pipe(
         // we'll wait for the result of refreshToken action
         filter(refreshed => refreshed !== null),
         switchMap(refreshed => {
           if (refreshed) {
+            this.isRefreshInProgress = false;
             return next.handle(req);
           }
           throw new Error('Token request error');
         }),
         catchError(() => {
           this.store.dispatch(fromAuth.forceLogout());
+          this.isRefreshInProgress = false;
           return EMPTY;
         })
       );
