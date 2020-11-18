bbdf95a3cec519ceaf21dc753e4807c8554f53f5
chan-dev <christian.m.austria@gmail.com>
2020-11-18 10:28:52 +0800

    feat(client): add tailwind dark and sepia mode

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -23,14 +23,30 @@ module.exports = {
         '-1/4': '-25%',
         '-3/4': '-75%',
       },
-    },
+    }
   },
   variants: {
-    display: ['responsive', 'hover', 'focus', 'group-hover'],
-    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
-    backgroundColor: ['responsive', 'hover', 'focus', 'disabled'],
+    display: ({ after }) => after(['group-hover']),
+    borderColor: ({ after }) => after([
+      'focus-within',
+      'dark', 'dark-focus', 'dark-focus-within',
+      'sepia', 'sepia-focus', 'sepia-focus-within'
+    ]),
+    backgroundColor: ({ after }) => after([
+      'disabled',
+      'dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd',
+      'sepia', 'sepia-hover', 'sepia-group-hover', 'sepia-even', 'sepia-odd'
+    ]),
+    textColor: [
+      'dark', 'dark-hover', 'dark-active', 'dark-placeholder',
+      'sepia', 'sepia-hover', 'sepia-active', 'sepia-placeholder'
+    ]
   },
-  plugins: [require('tailwindcss-truncate-multiline')()],
+  plugins: [
+    require('tailwindcss-truncate-multiline')(),
+    require('tailwindcss-dark-mode')(),
+    require('tailwindcss-sepia-mode')()
+  ],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
