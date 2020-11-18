37f74aed34bb91e06f34ec7ff177da024c094ca7
chan-dev <christian.m.austria@gmail.com>
2020-11-18 13:29:12 +0800

    feat(client): check color scheme preference on load
    Note that matchMedia listener doesn't run on page load so we add a call
    at app component to check during page load.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
+import { ThemeService } from './core/ui/services/theme.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'pocketlite';
+  constructor(private uiService: ThemeService) {}
+
+  ngAfterViewInit(): void {
+    // matchMedia doesn't run on page load
+    // basically, check on page load the current user preference
+    this.uiService.checkColorSchemePreference();
+  }
 }
