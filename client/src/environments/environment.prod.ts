e73dba3408b394515b33e3965af18d80a92302db
chan-dev <christian.m.austria@gmail.com>
2020-11-27 10:26:25 +0800

    fix(client): update oauth2 login url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  oauth2LoginUrl: 'https://pocketlite.herokuapp.com/api/auth/google',
+  oauth2LoginUrl: 'https://pocketlite-app.herokuapp.com/api/auth/google',
   rollbarConfig: {
     accessToken: 'ac097a1ba9f64cafbdaccd58caebd84b',
     captureUncaught: true,
